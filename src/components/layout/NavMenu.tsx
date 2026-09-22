eb756b2c3cdee11a7de7b32386f114ceae478667
Aidan Zazueta Medrano <aidanzm@outlook.com>
2026-09-22 13:58:29 -0700

    fix(needfinding): add needfinding to navbar (#81)
    Se agrego needfinding al navbar. Closes #80

--- src/components/layout/NavMenu.tsx
+++ src/components/layout/NavMenu.tsx
@@ -10,6 +10,7 @@ interface NavItem {
 const NAV_ITEMS: NavItem[] = [
   { label: "Entrevista a Expertos", path: "/expert-interviews" },
   { label: "Usuarios Extremos", path: "/extreme-users" },
+  { label: "Needfinding", path: "/needfinding" },
   { label: "Empathy Map", path: "/empathy-map" },
   { label: "Roper Dynagram", path: "/roper-dynagram" },
 ];
